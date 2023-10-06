8adc6bec75e5262af776673f2574968abadb3034
Ishaan Gurazada <eluruishaan@gmail.com>
2023-10-06 08:48:49 -0400

    init

---
+++ examples/20230807_12_37_34_Exported/dir_detect.h
@@ -0,0 +1,186 @@
+/**
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#ifndef DIR_DETECT_H
+#define DIR_DETECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#ifndef MEMS_UCF_SHARED_TYPES
+#define MEMS_UCF_SHARED_TYPES
+
+/** Common data block definition **/
+typedef struct {
+  uint8_t address;
+  uint8_t data;
+} ucf_line_t;
+
+#endif /* MEMS_UCF_SHARED_TYPES */
+
+/** Configuration array generated from Unico Tool **/
+const ucf_line_t dir_detect[] = {
+  {.address = 0x10, .data = 0x00,},
+  {.address = 0x11, .data = 0x00,},
+  {.address = 0x01, .data = 0x80,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x00,},
+  {.address = 0x17, .data = 0x40,},
+  {.address = 0x02, .data = 0x11,},
+  {.address = 0x08, .data = 0xEA,},
+  {.address = 0x09, .data = 0x46,},
+  {.address = 0x09, .data = 0x03,},
+  {.address = 0x09, .data = 0x50,},
+  {.address = 0x09, .data = 0x03,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0A,},
+  {.address = 0x02, .data = 0x11,},
+  {.address = 0x08, .data = 0xF2,},
+  {.address = 0x09, .data = 0x34,},
+  {.address = 0x02, .data = 0x11,},
+  {.address = 0x08, .data = 0xFA,},
+  {.address = 0x09, .data = 0x3C,},
+  {.address = 0x09, .data = 0x03,},
+  {.address = 0x09, .data = 0x52,},
+  {.address = 0x09, .data = 0x03,},
+  {.address = 0x09, .data = 0x5E,},
+  {.address = 0x09, .data = 0x03,},
+  {.address = 0x02, .data = 0x31,},
+  {.address = 0x08, .data = 0x3C,},
+  {.address = 0x09, .data = 0x3F,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x03,},
+  {.address = 0x09, .data = 0x14,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0xFC,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x7C,},
+  {.address = 0x09, .data = 0x1F,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x02, .data = 0x31,},
+  {.address = 0x08, .data = 0x52,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x01, .data = 0x00,},
+  {.address = 0x01, .data = 0x80,},
+  {.address = 0x17, .data = 0x40,},
+  {.address = 0x02, .data = 0x09,},
+  {.address = 0x08, .data = 0x00,},
+  {.address = 0x09, .data = 0x61,},
+  {.address = 0x09, .data = 0x4F,},
+  {.address = 0x09, .data = 0x08,},
+  {.address = 0x09, .data = 0x02,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x08,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x32,},
+  {.address = 0x09, .data = 0x4B,},
+  {.address = 0x09, .data = 0x03,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x08,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x6A,},
+  {.address = 0x09, .data = 0x40,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0B,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x72,},
+  {.address = 0x09, .data = 0x4D,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0A,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x8A,},
+  {.address = 0x09, .data = 0x4D,},
+  {.address = 0x09, .data = 0x05,},
+  {.address = 0x09, .data = 0x02,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0A,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x15,},
+  {.address = 0x09, .data = 0x4E,},
+  {.address = 0x09, .data = 0x06,},
+  {.address = 0x09, .data = 0x04,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0A,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x6F,},
+  {.address = 0x09, .data = 0x4E,},
+  {.address = 0x09, .data = 0x02,},
+  {.address = 0x09, .data = 0x0E,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x09,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x29,},
+  {.address = 0x09, .data = 0x4E,},
+  {.address = 0x09, .data = 0x01,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0B,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x61,},
+  {.address = 0x09, .data = 0x4F,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x02,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x0B,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x09, .data = 0x00,},
+  {.address = 0x01, .data = 0x80,},
+  {.address = 0x17, .data = 0x00,},
+  {.address = 0x04, .data = 0x00,},
+  {.address = 0x05, .data = 0x10,},
+  {.address = 0x03, .data = 0x02,},
+  {.address = 0x02, .data = 0x01,},
+  {.address = 0x01, .data = 0x00,},
+  {.address = 0x5E, .data = 0x02,},
+  {.address = 0x01, .data = 0x80,},
+  {.address = 0x0D, .data = 0x01,},
+  {.address = 0x60, .data = 0x35,},
+  {.address = 0x01, .data = 0x00,},
+  {.address = 0x10, .data = 0x44,},
+  {.address = 0x11, .data = 0x00,}
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DIR_DETECT_H */
+
